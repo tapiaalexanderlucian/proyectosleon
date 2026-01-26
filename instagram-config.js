789f88c5a70957fa9519cf8f6dac90160411e2a4
tapiaalexanderlucian <tapiaalexander761@gmail.com>
2026-01-25 21:41:10 -0500

    Add files via upload

---
+++ instagram-config.js
@@ -0,0 +1,19 @@
+// Configuración de publicaciones de Instagram
+// Agrega los URLs de tus posts aquí
+const INSTAGRAM_POSTS = [
+    {
+        url: "https://www.instagram.com/zenith_gymoficial/p/DTbnEdxiSZ6/",
+        caption: "Entrenamiento intenso 💪"
+    },
+    // Agrega más posts aquí en este formato:
+    // {
+    //     url: "https://www.instagram.com/zenith_gymoficial/p/XXXXX/",
+    //     caption: "Descripción del post"
+    // }
+];
+
+// Configuración de la cuenta
+const INSTAGRAM_CONFIG = {
+    username: "zenith_gymoficial",
+    instagramUrl: "https://www.instagram.com/zenith_gymoficial/"
+};
